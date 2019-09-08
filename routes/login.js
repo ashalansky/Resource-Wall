d7cd17c7056b11750dbc45ab85abc8d1cc5bfa01
Devon Blake <devon.blake@live.com>
2019-09-08 15:24:10 -0600

    login route completed

--- routes/login.js
+++ routes/login.js
@@ -7,3 +7,29 @@ module.exports = (db) => {
   });
   return router;
 };
+
+
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const queryString = `
+  SELECT * 
+  FROM users
+  WHERE username = $1`;
+  const queryParams = [username];
+  db.query(queryString, queryParams)
+  .then(data => data.rows)
+  .then(user => {
+    if (user.length) {
+      if (bcrypt.compareSync(password, user[0]["password"])) {
+        req.session.userId = user[0]["id"];
+        res.redirect("/");
+      } else {
+        res.render("login");
+      }
+    } else {
+      res.render("login");
+    }
+  });
+});
